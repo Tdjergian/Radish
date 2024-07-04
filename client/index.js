b52fac0a626e26103f1a5dc554782b5ccf76b613
Jay Hoogheem <jayh1186@gmail.com>
2024-07-03 22:05:03 -0500

    added react router and aws pricing form

--- client/index.js
+++ client/index.js
@@ -1,15 +1,26 @@
-
-import App from './App.tsx';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Redux/store.ts';
-import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import Main from './container/Main';
+import PricingPage from './Pages/PricingPage.jsx';
+import PricingForm from './components/PricingForm.jsx';
 import '../public/style.css';
-import React from 'react';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Main />} />
+            <Route path="/pricing" element={<PricingForm />} />
+          </Route>
+      </Routes>
+    </Router>
   </Provider>
 );
