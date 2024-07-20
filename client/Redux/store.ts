fd1ab3cba0ded617e0502988bd9552add62bd9c5
Jay Hoogheem <jayh1186@gmail.com>
2024-07-20 12:04:25 -0500

    updated store with user slice

--- client/Redux/store.ts
+++ client/Redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import redisFormSlice from './slices/redisFormSlice';
 import awsSlice from './slices/awsSlice';
 import sliderSlice from './slices/sliderSlice';
+import userSlice from './slices/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 const store = configureStore(
@@ -10,6 +11,7 @@ const store = configureStore(
       slider: sliderSlice,
       redis: redisFormSlice,
       aws: awsSlice,
+      user: userSlice,
     },
   }
 );
