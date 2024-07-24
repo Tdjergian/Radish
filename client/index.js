25ebdc0abd3bdcc4c39659d41ae9dac98743a670
basswoman <54603761+basswoman@users.noreply.github.com>
2024-07-24 13:05:15 +0300

    benchmark displayed

--- client/index.js
+++ client/index.js
@@ -1,28 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./Redux/store.ts";
-import App from "./App.tsx";
-import Main from "./container/Main";
-import PricingPage from "./Pages/PricingPage.jsx";
-import PricingForm from "./components/PricingForm.jsx";
-import Performance from "./components/Performance.tsx";
-import Register from "./components/Register.jsx"
-import Login from "./components/Login.jsx"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './Redux/store.ts';
+import App from './App.tsx';
+import Main from './container/Main';
+import PricingPage from './Pages/PricingPage.jsx';
+import PricingForm from './components/PricingForm.jsx';
+import Performance from './components/Performance.tsx';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
 
+import 'react-tooltip/dist/react-tooltip.css';
+import '../public/style.css';
 
-import "react-tooltip/dist/react-tooltip.css";
-import "../public/style.css";
+import 'react-tooltip/dist/react-tooltip.css';
+import LandingPage from './components/LandingPage.jsx';
+import Slider from './components/Slider';
 
-import Register from "./components/Register.jsx";
-import Login from "./components/Login.jsx";
-
-import "react-tooltip/dist/react-tooltip.css";
-import LandingPage from "./components/LandingPage.jsx";
-import Slider from "./components/Slider";
-
-const container = document.getElementById("app");
+const container = document.getElementById('app');
 const root = createRoot(container);
 
 root.render(
